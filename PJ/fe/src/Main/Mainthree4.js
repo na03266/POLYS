dc9b81456e24e239c9d56649657464932b0d307a
jijini <irrjin00@naver.com>
2023-10-03 17:11:21 +0900

    123

---
+++ PJ/fe/src/Main/Mainthree4.js
@@ -0,0 +1,37 @@
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+
+function Mainthree() {
+  const sceneRef = useRef();
+  const camera = new THREE.PerspectiveCamera(75, 300 / 500, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer();
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    renderer.setSize(300, 500); // 크기 조정
+    sceneRef.current.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    renderer.setClearColor(0xffffff); // 0xffffff는 흰색을 나타내는 16진수입니다.
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // 카메라 위치 조정
+    camera.position.set(0, 0, 2);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  }, []);
+
+  return <div ref={sceneRef}></div>;
+}
+
+export default Mainthree;
--- PJ/fe/src/Main/ThreeBox.js
+++ PJ/fe/src/Main/ThreeBox.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import MainThree from './Mainthree';
 import Mainthree2 from './Mainthree2';
-
+import Mainthree3 from './Mainthree3';
+import Mainthree4 from './Mainthree4';
+import Mainthree5 from './Mainthree5';
+ 
 function ThreeBox() {
   const containerStyle = {
     display: 'flex', // Flexbox 사용
@@ -12,6 +15,9 @@ function ThreeBox() {
     <div style={containerStyle}>
       <MainThree />
       <Mainthree2 />
+      <Mainthree3 />
+      <Mainthree4 />
+      <Mainthree5 />
     </div>
   );
 }
