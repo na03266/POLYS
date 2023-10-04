64979796f3886072bf788dada49be970503c95a6
sbJeon7 <121412350+sbJeon7@users.noreply.github.com>
2023-10-04 09:50:46 +0900

    회원가입 하고있던거 일단 커밋..

--- PJ/fe/src/Regist/RegistCustomizing.js
+++ PJ/fe/src/Regist/RegistCustomizing.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { AiFillCaretLeft } from "react-icons/ai"
+import { AiFillCaretRight } from "react-icons/ai"
+
 
 function RegistCustomizing() {
+  const handleRegistWait = () => {               
+    window.location.href='/registWait';
+  };
+  const handleRegistLast = () => {               
+    window.location.href='/registLast';
+  };
+
   return (
-    <div>RegistCustomizing</div>
+    <div>
+      <button onClick={handleRegistWait}>이전</button>
+      <button onClick={handleRegistLast}>다음</button>
+      <br/ >
+      <button className='cm_bt'>
+        <AiFillCaretLeft />
+      </button>
+      머리1
+      <button className='cm_bt'>
+        <AiFillCaretRight />
+      </button>
+      <br/ >
+      <button className='cm_bt'>
+        <AiFillCaretLeft />
+      </button>
+      표정1
+      <button className='cm_bt'>
+        <AiFillCaretRight />
+      </button>
+    </div>
   )
 }
 
--- PJ/fe/src/Regist/RegistWait.js
+++ PJ/fe/src/Regist/RegistWait.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function RegistWait() {
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+  const redirectTimer = setTimeout(() => {
+    localStorage.removeItem('studentName');
+    window.location.href = '/registCustomizing';
+  }, 5000);
+  return () => {  
+    clearTimeout(redirectTimer);
+  };
+}, []);
+
   return (
-    <div>RegistWait</div>
+    <div>
+      <div>왼쪽 정면 오른쪽</div>
+    인식중...
+    <br/ >
+    움직이지 말고
+    <br/ >
+    정면을 보세요
+    <h3>{countdown > 0 ? `${countdown}초 뒤에 홈 화면으로 이동합니다.` : '이동 중...'}</h3>
+    </div>
   )
 }
 
