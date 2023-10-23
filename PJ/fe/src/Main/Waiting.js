a5b674f58e75bc23b09cc0bdff6d3ef724ed4dad
kangsiii <lks405jj@naver.com>
2023-10-24 08:18:29 +0900

    123

--- PJ/fe/src/Main/Waiting.js
+++ PJ/fe/src/Main/Waiting.js
@@ -12,9 +12,13 @@ function Waiting() {
       <div onClick={goToHomePage}>
         <Clock />
         <img src={'/pol.png'} style={{ maxWidth: '40%', height: 'auto', marginLeft: '30%' }} />
-        <h1 style={{ textAlign: 'center', marginTop: '5%' }}>출석부시스템 바로가기</h1>
+        <h1 style={{ textAlign: 'center', marginTop: '5%' }}>AI소프트웨어 출석부시스템</h1>
         <img src={'/QRGuide.png'} style={{ maxWidth: '40%', height: 'auto', marginLeft: '30%' }} />
         <h1 style={{ textAlign: 'center', marginTop: '5%' }}>가이드</h1>
+        <h1 style={{ textAlign: 'center'}}>QR코드를 이용하기 위해서는<br></br>Ai_5G 와이파이에 연결해주세요<br></br><br></br>
+          WIFI : Ai_5G<br></br>
+          PW : 000000AI
+        </h1>
       </div>
     </div>
   );
